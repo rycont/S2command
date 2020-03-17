5ad575140963c29620be9904a5264043705db1af
reactericdev <reactdev@kakao.com>
2020-03-17 23:44:13 +0900

    (fix) remove unused parameter (adding keyshortcuts) 단축키 등록하는 익명함수에 안쓰는 인자 제거했어요

--- src/bg.ts
+++ src/bg.ts
@@ -5,7 +5,7 @@ chrome.storage.sync.get('shortcuts', ({shortcuts: rawShortcuts}) => {
     const shortcuts: ShortcutsT = JSON.parse(rawShortcuts)
     Object.keys(shortcuts).filter(uri => RegExp(uri).test(location.href)).forEach(uri => {
         Object.keys(shortcuts[uri]).forEach(key => {
-            hotkeys(key, (event) => eval(shortcuts[uri][key]))
+            hotkeys(key, () => eval(shortcuts[uri][key]))
         })
     })
 })
